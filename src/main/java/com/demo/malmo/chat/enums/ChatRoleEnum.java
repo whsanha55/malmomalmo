37cff5077a9e4ec84bd50e96b194e0b47a51baa8
whsanha55 <whsanha55@gmail.com>
2024-07-25 00:08:36 +0900

    ChatRoleEnum hyperclova token 제거

--- src/main/java/com/demo/malmo/chat/enums/ChatRoleEnum.java
+++ src/main/java/com/demo/malmo/chat/enums/ChatRoleEnum.java
@@ -1,23 +1,14 @@
 package com.demo.malmo.chat.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum ChatRoleEnum {
 
-    // TODO: 7/25/24 token 분리 
-    WHITE_HAT("NTA0MjU2MWZlZTcxNDJiYyBdmB26fM9O5XCDoDkBgCxhAOyFLsqP9K2m0yJ7b1g9", "T1uJ6v9Mnos2RRUCtaDaqABiGNjX0T0WmKL7NNcu"),
-    BLACK_HAT("NTA0MjU2MWZlZTcxNDJiYyBdmB26fM9O5XCDoDkBgCxhAOyFLsqP9K2m0yJ7b1g9", "T1uJ6v9Mnos2RRUCtaDaqABiGNjX0T0WmKL7NNcu"),
-    GREEN_HAT("NTA0MjU2MWZlZTcxNDJiYyBdmB26fM9O5XCDoDkBgCxhAOyFLsqP9K2m0yJ7b1g9", "T1uJ6v9Mnos2RRUCtaDaqABiGNjX0T0WmKL7NNcu"),
-    YELLOW_HAT("NTA0MjU2MWZlZTcxNDJiYyBdmB26fM9O5XCDoDkBgCxhAOyFLsqP9K2m0yJ7b1g9", "T1uJ6v9Mnos2RRUCtaDaqABiGNjX0T0WmKL7NNcu"),
-    RED_HAT("NTA0MjU2MWZlZTcxNDJiYyBdmB26fM9O5XCDoDkBgCxhAOyFLsqP9K2m0yJ7b1g9", "T1uJ6v9Mnos2RRUCtaDaqABiGNjX0T0WmKL7NNcu"),
-    BLUE_HAT("NTA0MjU2MWZlZTcxNDJiYyBdmB26fM9O5XCDoDkBgCxhAOyFLsqP9K2m0yJ7b1g9", "T1uJ6v9Mnos2RRUCtaDaqABiGNjX0T0WmKL7NNcu"),
-    BLUE_HAT_BEGIN("NTA0MjU2MWZlZTcxNDJiYyBdmB26fM9O5XCDoDkBgCxhAOyFLsqP9K2m0yJ7b1g9", "T1uJ6v9Mnos2RRUCtaDaqABiGNjX0T0WmKL7NNcu"),
+    WHITE_HAT,
+    BLACK_HAT,
+    GREEN_HAT,
+    YELLOW_HAT,
+    RED_HAT,
+    BLUE_HAT,
+    BLUE_HAT_BEGIN,
     ;
 
-
-    private final String clovaKey;
-    private final String gatewayKey;
 }
