7139908049007e44a2b07644e2625b534c384d2e
whsanha55 <whsanha55@gmail.com>
2024-07-26 22:16:54 +0900

    swagger 에서 정상적으로 url 접근이 안되는 문제 수정

--- src/main/java/com/demo/malmo/MainApplication.java
+++ src/main/java/com/demo/malmo/MainApplication.java
@@ -1,5 +1,7 @@
 package com.demo.malmo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -8,6 +10,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties
 @SpringBootApplication
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "/", description = "Default Server URL")
+    }
+)
+
 public class MainApplication {
 
     public static void main(String[] args) {
