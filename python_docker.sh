ef9dba1114edf9f53cd266f7d32ec04dc5040369
whsanha55 <whsanha55@gmail.com>
2024-07-28 12:21:55 +0900

    python_docker.sh ssh sh 스크립트 전에 10초 쉬기

--- python_docker.sh
+++ python_docker.sh
@@ -8,5 +8,12 @@ docker tag malmomalmo jjong5/malmomalmo:latest
 # 이미지 push
 docker push jjong5/malmomalmo:latest
 
+# 10초 대기하면서 카운트 출력
+echo "Waiting for 10 seconds..."
+for i in {10..1}; do
+    echo "$i seconds remaining..."
+    sleep 1
+done
+
 # 원격 서버에 python_startup.sh 스크립트 파일 실행
 sshpass -p 'Y8@!P@-4MyHce' ssh root@223.130.157.144 "bash -c 'sh /home/python/startup.sh'"
