88f93a67337f9a7ac02be968cd68348d05957880
Jongha Hwang <34565744+whsanha55@users.noreply.github.com>
2024-09-02 11:05:36 +0900

    Delete to_server.sh

--- to_server.sh
+++
@@ -1,3 +0,0 @@
-gradle build
-sshpass -p 'Y8@!P@-4MyHce' scp build/libs/malmomalmo.jar root@223.130.157.144:/home/malmomalmo/
-sshpass -p 'Y8@!P@-4MyHce' ssh root@223.130.157.144 "bash -c 'sh /home/malmomalmo/startup.sh'"
